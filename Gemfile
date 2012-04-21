c08b6f73241e46ec8d2380a29a70c83b41676706
Wilfried Deluche <wilfried.deluche@gmail.com>
2012-04-21 16:12:26 +0200

    Devise installed, initialized and linked to User model

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.3'
 gem 'pg'
 gem 'jquery-rails'
 gem 'json'
+gem 'devise'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module ErpProject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.action_mailer.default_url_options = { :host => Rails.env == "production" ? "private.ingesup.com" : "localhost:3000" }
   end
 end
