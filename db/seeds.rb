bbb77efa63309b49b6262b7093a7ee39add0d755
Wilfried Deluche <wilfried.deluche@gmail.com>
2012-05-01 19:19:39 +0200

    Devise invitable installed - email and views translated

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ puts "#### SCHOOL USER ####"
 school_user = SchoolUser.create!
 user = User.create! :email => 'pedago@ingesup.com', :password => 'ingesup', :password_confirmation => 'ingesup', :first_name => 'Prenom', :last_name => 'Nom' do |u|
   u.rolable = school_user
+  u.invitation_accepted_at = DateTime.now
 end
 puts user.first_name << " " << user.last_name << " as " << user.rolable_type
 
@@ -22,5 +23,6 @@ puts "#### TEACHER ####"
 teacher = Teacher.create! :arrival_year => "2006"
 user2 = User.create! :email => 'teacher@ingesup.com', :password => 'ingesup', :password_confirmation => 'ingesup', :first_name => 'Teacher', :last_name => 'Nom' do |u|
   u.rolable = teacher
+  u.invitation_accepted_at = DateTime.now
 end
 puts user2.first_name << " " << user2.last_name << " as " << user2.rolable_type
