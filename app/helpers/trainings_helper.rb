f5bc72585122f7804d643de7a16f2f63d3218dd4
Wilfried Deluche <wilfried.deluche@gmail.com>
2012-06-22 17:06:50 +0200

    Add select box for training level and class year

--- app/helpers/trainings_helper.rb
+++ app/helpers/trainings_helper.rb
@@ -1,2 +1,7 @@
+# encoding: utf-8
 module TrainingsHelper
+  
+  def submit_text(training)
+    (training.persisted?) ? "Mettre à jour" : "Créer"
+  end
 end
--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Klass < ActiveRecord::Base
     
   belongs_to :training
