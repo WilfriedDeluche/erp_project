a661c9049b553da595085ab896e1a40a7597be55
Wilfried Deluche <wilfried.deluche@gmail.com>
2012-06-24 19:31:18 +0200

    Modified helper for user label

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,14 +2,14 @@
 module StudentsHelper
   
   def student_is_captain(student)
-    return unless student.is_captain
+    return unless student.is_captain?
     content_tag(:span, :class => "label label-info") do
       "Délégué"
     end
   end
   
   def student_is_student_union_member(student)
-    return unless student.is_student_union_member
+    return unless student.is_student_union_member?
     content_tag(:span, :class => "label") do
       "Membre du BDE"
     end
